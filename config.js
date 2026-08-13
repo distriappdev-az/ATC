72684d76f3a9953d0f7e274cc555ef71275bd159
distriappdev-az <distriappdev@gmail.com>
2026-08-13 11:41:01 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+  URL_APPS_SCRIPT:
+    "https://script.google.com/macros/s/AKfycbw7nzQdL7Kmc_cVBNp9FUPWlBwvuibc2v2vonb8sdBjxYtTuejJsJ7of03W7ZYkvAen4A/exec",
+
+  FOTOS_FOLDER_ID:
+    "1xC8tprJmkdNfFx1-SjhT051S-99NMRUV",
+
+  ARCHIVOS_FOLDER_ID:
+    "12pNH7BGWnw2umhSWPq4cGQmW_kQ8Jc1A"
+};
