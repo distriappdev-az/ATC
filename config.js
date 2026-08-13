25e9756c2b9006489e819ac9270931acccbb185f
distriappdev-az <distriappdev@gmail.com>
2026-08-13 14:07:41 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   URL_APPS_SCRIPT:
-    "https://script.google.com/macros/s/AKfycbw7nzQdL7Kmc_cVBNp9FUPWlBwvuibc2v2vonb8sdBjxYtTuejJsJ7of03W7ZYkvAen4A/exec",
+    "https://script.google.com/macros/s/AKfycbyPhqSDEdAtBlBxsz6Y02LDlcnl_KhXPsvL30aBppHlTN5aFeg3zMp_3pOgLm4u0Emjww/exec",
 
   FOTOS_FOLDER_ID:
     "1xC8tprJmkdNfFx1-SjhT051S-99NMRUV",
